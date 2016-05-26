d42c181d996d5b47e3adb138660f0643f5a57572
Alex Smirnov <alexsmirnov765@gmail.com>
2016-05-26 01:54:01 +0000

    Fixed typo in Registration Controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,11 +3,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def create
         super do |resource|
             if params[:plan]
-                resource plan_id = params[:plan]
-                if resource plan_id == 2
-                    resource save_with_payment
+                resource.plan_id = params[:plan]
+                if resource.plan_id == 2
+                    resource.save_with_payment
                 else
-                    resource save
+                    resource.save
                 end    
             end    
         end    
