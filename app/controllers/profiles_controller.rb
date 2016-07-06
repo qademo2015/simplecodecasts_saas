4f19f965f25421edb62d6396e582e7be21b88de8
Alex Smirnov <alexsmirnov765@gmail.com>
2016-07-06 01:19:15 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
 class ProfilesController < ApplicationController
+    #next row will force user authentication before performing any type of activity
+    before_action :authenticate_user!
+    
+    #next row will prevent user from being able to edit/change somebody else's data
+    before_action :only_current_user
     
     def new
         # form where a user can fill out their own profile.
@@ -20,11 +25,30 @@ class ProfilesController < ApplicationController
     def edit
         @user = User.find( params[:user_id] )
         @profile = @user.profile
+    end
+    
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        #next block will redirect user to his main page in case of successfull update
+        #user's profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = 'Profile updated!'        
+            redirect_to user_path( params[:user_id] )
+        else
+            #nect row will redirect user to the Edit page in case if failure
+            render action: :edit
+        end    
     end    
     
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+        
+        def only_current_user
+            @user = User.find( params[:user_id] )
+            redirect_to(root_url) unless @user == current_user
         end    
     
 end    
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    #next row will force user authentication before performing any type of activity
+    before_action :authenticate_user!
+    
     def show
         @user = User.find( params[:id] )
     end
