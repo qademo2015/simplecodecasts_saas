5eb252c0b87e770d5f456ae8d10e6f6a472a4f24
Alex Smirnov <alexsmirnov765@gmail.com>
2016-08-04 01:53:02 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
     #next row will force user authentication before performing any type of activity
     before_action :authenticate_user!
     
+    # method below will be used to show all community members.
+    # The corresponding view should be titled 'index.html.erb'
+    # and be located in 'app/views/users' folder
+    def index
+    
+    end    
+    
+    
     def show
         @user = User.find( params[:id] )
     end
